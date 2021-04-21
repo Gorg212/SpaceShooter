207610e19de32b7d8e42a3296d546bc2790b2990
Gorg212 <thewakebakecake@gmail.com>
2021-04-21 23:21:07 +0530

    Update: Now Spaceships can't go out of the window.

--- game.py
+++ game.py
@@ -23,6 +23,16 @@ def draw_window(S1, S2):
   WIN.blit(Spaceship2, (S2.x, S2.y))
   pygame.display.update()
 
+def S1_control(This_key_is_pressed, S1):
+    if This_key_is_pressed[pygame.K_UP] and S1.y - 2 > 0:
+        S1.y -= 2
+    if This_key_is_pressed[pygame.K_DOWN] and S1.y + 2 + S1.height < height:
+        S1.y += 2
+    if This_key_is_pressed[pygame.K_LEFT] and S1.x - 2 > 0:
+        S1.x -= 2
+    if This_key_is_pressed[pygame.K_RIGHT] and S1.x + 2 + S1.width < width:
+        S1.x += 2
+        
 def main():
     S1 = pygame.Rect(250, 230, S1_W, S1_H)
     S2 = pygame.Rect(693, 230, S2_W, S2_H)
@@ -34,21 +44,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        This_key_is_pressed = pygame.key.get_pressed()       
-
-        if This_key_is_pressed[pygame.K_UP]:
-            S1.y -= 2
-        if This_key_is_pressed[pygame.K_DOWN]:
-            S1.y += 2
-        if This_key_is_pressed[pygame.K_LEFT]:
-            S1.x -= 2
-        if This_key_is_pressed[pygame.K_RIGHT]:
-            S1.x += 2
-        
-        
-        # S1.x += 1
-        # S2.x -= 1
+        This_key_is_pressed = pygame.key.get_pressed() 
         draw_window(S1, S2)
+        S1_control(This_key_is_pressed, S1)
     pygame.quit()
 
 if __name__ == '__main__':
