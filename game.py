f04e5cbe6a109de6e52ef9993c7e1c90986beb36
Gorg212 <thewakebakecake@gmail.com>
2021-04-25 14:28:26 +0530

    e

--- game.py
+++ game.py
@@ -7,32 +7,37 @@ pygame.display.set_caption("Gorg's Kool SpaceShooter 69420")
 # pygame.display.set_icon("""E:\Some photos n wallpaers\pfp\kool pfp.jpg""")
 
 # Some values
-GREYISH_BLACK = (38, 38, 38)
+GREY = (38, 38, 38)
 FPS = 100 # Specify the FPS you want to fix here
-Spaceship1 = pygame.image.load(os.path.join('SpaceShooter', 'Assets', 'SpaceShip.png'))
-Spaceship2 = pygame.image.load(os.path.join('SpaceShooter', 'Assets', 'SpaceShip2.png'))
-
+ShipSpeed = 3 # Speed of the SpaceShip
+Spaceship1 = pygame.image.load(os.path.join('SpaceShooter', 'Assets', 'SpaceShip.png')) # Spaceship1
+Spaceship2 = pygame.image.load(os.path.join('SpaceShooter', 'Assets', 'SpaceShip2.png')) # Spaceship2
+# SPACESHIP1 HITBOX
 S1_W = 30
 S1_H = 30
-
+# SPACESHIP1 HITBOX
 S2_W = 30
 S2_H = 30
+# Loads all sprites
 def draw_window(S1, S2):
-  WIN.fill(GREYISH_BLACK)
+  WIN.fill(GREY)
   WIN.blit(Spaceship1, (S1.x, S1.y))
   WIN.blit(Spaceship2, (S2.x, S2.y))
   pygame.display.update()
 
+# Controls to move Spaceship
 def S1_control(This_key_is_pressed, S1):
-    if This_key_is_pressed[pygame.K_UP] and S1.y - 2 > 0:
-        S1.y -= 2
-    if This_key_is_pressed[pygame.K_DOWN] and S1.y + 2 + S1.height < height:
-        S1.y += 2
-    if This_key_is_pressed[pygame.K_LEFT] and S1.x - 2 > 0:
-        S1.x -= 2
-    if This_key_is_pressed[pygame.K_RIGHT] and S1.x + 2 + S1.width < width:
-        S1.x += 2
-        
+    if This_key_is_pressed[pygame.K_UP] and S1.y - ShipSpeed > 0:
+        S1.y -= ShipSpeed
+    if This_key_is_pressed[pygame.K_DOWN] and S1.y + ShipSpeed + S1.height < height:
+        S1.y += ShipSpeed
+    if This_key_is_pressed[pygame.K_LEFT] and S1.x - ShipSpeed > 0:
+        S1.x -= ShipSpeed
+    if This_key_is_pressed[pygame.K_RIGHT] and S1.x + ShipSpeed + S1.width < width:
+        S1.x += ShipSpeed
+
+
+# Main Function. This is where all the magic happens!
 def main():
     S1 = pygame.Rect(250, 230, S1_W, S1_H)
     S2 = pygame.Rect(693, 230, S2_W, S2_H)
